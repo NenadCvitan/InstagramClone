361ee33f07a1f71a60b703e73293994828a9f972
NenadCvitan <cvitanweb@gmail.com>
2024-10-22 00:14:27 +0200

    Init and upload Backend for clone

---
+++ BackendInstagramClone/instaclone-service/src/main/java/InstaFiction/instaclone_service/InstacloneServiceApplication.java
@@ -0,0 +1,13 @@
+package InstaFiction.instaclone_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstacloneServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstacloneServiceApplication.class, args);
+	}
+
+}
---
+++ BackendInstagramClone/instaclone-service/src/test/java/InstaFiction/instaclone_service/InstacloneServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package InstaFiction.instaclone_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstacloneServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
