08b2d9d549bb304ff4029abc76697f27b631ceb7
NenadCvitan <cvitanweb@gmail.com>
2024-10-22 15:55:18 +0200

    finished responsive design

--- FrontendInstagramClone/loginpage/loginScript.js
+++ FrontendInstagramClone/loginpage/loginScript.js
@@ -1,3 +1,24 @@
 function func(){
- alert("Hallo , Welt");
+ document.getElementById("loginButton").addEventListener('click' , function(){
+
+    const name = document.getElementById('inputName').value;
+
+    fetch('http://localhost:8080/api/login',{
+        method : 'Post' ,
+        headers : {
+            'Content-Type' : 'application/json',
+
+        },
+        body : JSON.stringify({
+          username : name  
+        }),
+    }) .then(response => response.json())
+    .then(data => {
+        console.log('Succes' , data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+
+ });
 }
--- FrontendInstagramClone/registerpage/register.js
+++ FrontendInstagramClone/registerpage/register.js
@@ -1,3 +1,40 @@
-function register(){
-    alert("Test")
+function register() {
+    document.getElementById("loginButton").addEventListener('click', function() {
+        const name = document.getElementById('inputNumberOrName').value;
+        const password = document.getElementById('inputNewPassword').value;
+        const fullName = document.getElementById('inputFullName').value;
+        const username = document.getElementById('inputNewUsername').value;
+
+        console.log(name);
+        console.log(password);
+        console.log(fullName);
+        console.log(username);
+
+        fetch('http://localhost:8080/api/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ 
+                usernameOrNumber: name, 
+                password: password, 
+                fullName: fullName, 
+                username: username 
+            }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    });
 }
+
+
+
+     
+
+
+
